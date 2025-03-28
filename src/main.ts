8ae65286b0decdba1dff34df6b04ad5ed5ac4c98
Pedro Ollero <pedroollero1998@gmail.com>
2025-03-28 09:30:43 +0100

    Add password validation logic and common passwords list

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,61 @@
-import "./style.css";
+import { ValidacionClave } from "./modal";
 
-console.log("Hello Typescript!");
+const tieneMayusculasYMinusculas = (clave: string): ValidacionClave => {
+  let minusculas: string = "qwertyuiopasdfghjklzxcvbnm";
+  let mayusculas: string = "QWERTYUIOPASDFGHJKLZXCVBNM";
+  let contraseñaArray: string[] = clave.split("");
+
+  const hayCoincidenciaMinus = contraseñaArray.some((item) =>
+    minusculas.includes(item)
+  );
+  const hayCoincidenciaMayus = contraseñaArray.some((item) =>
+    mayusculas.includes(item)
+  );
+
+  console.log(hayCoincidenciaMayus)
+  console.log(hayCoincidenciaMinus)
+
+  if (
+    hayCoincidenciaMayus === true &&
+    hayCoincidenciaMinus === true
+  ) {
+    return {
+      esValida: true,
+    };
+  }
+  if (
+    hayCoincidenciaMayus === false &&
+    hayCoincidenciaMinus === false
+  ) {
+    return {
+      esValida: false,
+      error: "La contraseña debe contener al menos una mayuscula y una minuscula"
+    };
+  }
+  if (
+    hayCoincidenciaMayus === true &&
+    hayCoincidenciaMinus === false
+  ) {
+    return {
+      esValida: false,
+      error: "No contiene al menos una minuscula"
+    };
+  }
+  if (
+    hayCoincidenciaMayus === false &&
+    hayCoincidenciaMinus === true
+  ) {
+    return {
+      esValida: false,
+      error: "No contiene al menos una mayuscula"
+    };
+  }
+  
+  return{
+    esValida: false,
+    error: "Contraseña incorrecta"
+  };
+};
+
+let resultado = tieneMayusculasYMinusculas("asd")
+console.log(resultado)
